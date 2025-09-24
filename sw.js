2e37c5312fcf8391803d306becb57a522f557c03
i-sirius <pollux.twin@gmail.com>
2025-09-24 21:41:23 +0300

    Add PWA: manifest + service worker

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = "svitlotycyalka-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  // cache-first для статичних ресурсів
+  e.respondWith(
+    caches.match(req).then(cached => cached || fetch(req))
+  );
+});
